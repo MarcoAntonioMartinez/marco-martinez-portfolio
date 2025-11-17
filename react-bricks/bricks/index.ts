f0a7a27a53e7af00aac968149387928c9b2ab7bc
MarcoAntonioMartinez <marco.antoni.martinez@gmail.com>
2025-11-17 17:38:37 -0600

    Removed references to deleted brick

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -1,7 +1,7 @@
 import { types } from 'react-bricks/rsc'
 
 import HeroUnit from './custom/MyHeroUnit'
-import Pokemon from './custom/Pokemon'
+
 // import RegisterBrick from './custom/RegisterBrick/RegisterBrick'
 import reactBricksUITheme from './react-bricks-ui'
 
@@ -12,7 +12,7 @@ const bricks: types.Theme[] = [
     categories: [
       {
         categoryName: 'Custom bricks',
-        bricks: [HeroUnit, Pokemon], // Custom Bricks
+        bricks: [HeroUnit], // Custom Bricks
       },
     ],
   },
